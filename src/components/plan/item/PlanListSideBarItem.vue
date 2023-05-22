d5c9a4c7adc592bf8e8f09b0dd6a278bf8f6efe9
김세혁 <se6816@naver.com>
2023-05-22 11:23:57 +0900

    리스트 드래그 기능 추가

--- src/components/plan/item/PlanListSideBarItem.vue
+++ src/components/plan/item/PlanListSideBarItem.vue
@@ -34,30 +34,6 @@ export default {
     removeAttraction() {
       this.deleteAttraction(this.attraction);
     },
-    addDragging(event) {
-      event.target.classList.add("dragging");
-    },
-    endDragging(event) {
-      const prevDiv = this.getDragPrevElement(event.clientY);
-      event.target.classList.remove("dragging");
-      this.$emit("re-sort", event.target, prevDiv);
-    },
-    getDragPrevElement(y) {
-      const arr = [...document.querySelectorAll(".draggable:not(.dragging)")];
-      return arr.reduce(
-        (closest, child) => {
-          const box = child.getBoundingClientRect(); //해당 엘리먼트에 top값, height값 담겨져 있는 메소드를 호출해 box변수에 할당
-          const offset = y - box.top - box.height / 2; //수직 좌표 - top값 - height값 / 2의 연산을 통해서 offset변수에 할당
-          if (offset < 0 && offset > closest.offset) {
-            // (예외 처리) 0 이하 와, 음의 무한대 사이에 조건
-            return { offset: offset, element: child }; // Element를 리턴
-          } else {
-            return closest;
-          }
-        },
-        { offset: Number.NEGATIVE_INFINITY }
-      ).element;
-    },
   },
 };
 </script>
